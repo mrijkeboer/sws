4476b2f893bb333f6f72c0843d751ad824dcbfb0
Martijn P. Rijkeboer <martijn@bunix.org>
2011-07-23 14:11:55 +0200

    Simplify start script.

--- sws.sh
+++ sws.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 
 SELF=`/usr/bin/basename $0`
+ERL=`which erl`
 
 SNAME="sws-001"
 HOSTNAME="localhost"
 
-SWS_DIR="/opt/sws"
-SWS_USER="sws"
-
-ERL="/usr/bin/erl"
+SWS_DIR=$(cd ${0%/*} && pwd)
 EBINS="$SWS_DIR/apps/*/ebin $SWS_DIR/deps/*/ebin"
-SUDO="`which sudo` -u $SWS_USER"
 
 start() {
 	${ERL} -pa ${EBINS} -boot start_sasl -s reloader -s sws
 }
 
 stop() {
-	${SUDO} ${ERL} -noshell -pa ${EBINS} -sname ${SNAME}_stop -s sws stop ${SNAME}@${HOSTNAME}
+	${ERL} -noshell -pa ${EBINS} -sname ${SNAME}_stop -s sws stop ${SNAME}@${HOSTNAME}
 }
 
 shell() {
-	${SUDO} ${ERL} -sname ${SNAME}_shell -remsh ${SNAME}@${HOSTNAME}
+	${ERL} -sname ${SNAME}_shell -remsh ${SNAME}@${HOSTNAME}
 }
 
 case $1 in
